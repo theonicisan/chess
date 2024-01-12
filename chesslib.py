1a0ec1cfca106eb25a61036dca4a9c0e60452538
theonicisan <theo@spickett.com>
2024-01-12 12:51:37 +0200

    Testing bug-fixes related to the Replay Buffer,

--- chesslib.py
+++ chesslib.py
@@ -12,13 +12,13 @@ def play_game(in_board, training=None):
     # Debugging only.
     i_count = 0
     
-    #opening_position = 'dymmy' #board.peek()
+    # Initialize the first board position array...
     board_array = board_to_array(board)
-    #replay_buffer = np.zeros([1,9,8,12], dtype=int)
+    # ...and assign it to the replay buffer.
     replay_buffer = np.expand_dims(board_array, axis=0)
 
-    while not board.is_checkmate() and not board.is_stalemate() and not board.is_insufficient_material() and not board.can_claim_draw()    :
-        i_count+=1
+    while not board.is_checkmate() and not board.is_stalemate() and not board.is_insufficient_material() and not board.can_claim_draw() and not board.is_seventyfive_moves() and not board.is_fivefold_repetition():
+        #i_count+=1
         all_moves = str(board.legal_moves)
         start_pos = [pos for pos, char in enumerate(all_moves) if char == '(']
         end_pos = [pos for pos, char in enumerate(all_moves) if char == ')']
@@ -27,8 +27,24 @@ def play_game(in_board, training=None):
         all_moves_array = all_moves_string.split(',')
         next_move = random.choice(all_moves_array)
         board.push_san(next_move)
+        
+        # Convert the board to an array...
         board_array = board_to_array(board)
+        # ...and append it to the Replay Buffer
         replay_buffer = np.append(replay_buffer, np.expand_dims(board_array, axis=0), axis=0)
+
+    # Assign the reward in the Replay Buffer
+    if board_array[8][0][6] == 1:  # 1 if Checkmate
+        if board_array[8][0][1] == 0:
+            reward = -1
+        else:
+            reward = 1
+    else:
+        reward = 0
+
+    board_array[8][0][8] = reward
+    replay_buffer[1][8][0][8] = reward
+
     
     return board, replay_buffer
 
@@ -55,7 +71,7 @@ def simulate_play(iterations):
     start_time = time.time()
 
     for i in range(num_games):
-        out_board, _, _ = play_game(None)
+        out_board, _ = play_game(None)
         moves += out_board.fullmove_number
         if out_board.is_variant_draw() or out_board.can_claim_draw() or out_board.is_insufficient_material() or out_board.is_stalemate() or out_board.is_seventyfive_moves() or out_board.is_fivefold_repetition() :
             num_draws += 1
@@ -181,31 +197,6 @@ def board_to_array(in_board):
         elif board.is_variant_draw() or board.can_claim_draw() or board.is_insufficient_material() or board.is_stalemate() or board.is_seventyfive_moves() or board.is_fivefold_repetition():
             board_array[8][0][7] = 1 # 1 if board is draw
 
+        # Note: board_array[8][0][8] is the replay biuffer reward of -1, 0 or 1.
+
     return board_array
-
-def board_is_terminal(in_board):
-    import chess
-    # INPUTS
-    # in_board of type chess.board()
-
-    # OUTPUTS
-    # is_terminal as Boolean
-    # reward as int
-
-    board = chess.Board()
-    board = in_board
-
-    if board.is_variant_draw() or board.can_claim_draw() or board.is_insufficient_material() or board.is_stalemate() or board.is_seventyfive_moves() or board.is_fivefold_repetition():
-        is_terminal = True
-        reward = 0
-    elif board.is_checkmate():
-        is_terminal = True
-        if board.turn:
-            reward = -1
-        else:
-            reward = 1
-    else:
-        is_terminal = False
-        reward = None
-
-    return is_terminal, reward
